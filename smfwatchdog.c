72fd897724fed2c940da7f83f7cd3843557f6b72
Dave Eddy <dave@daveeddy.com>
2013-06-18 11:00:49 -0700

    bump

--- smfwatchdog.c
+++ smfwatchdog.c
@@ -25,7 +25,7 @@
 #include <unistd.h>
 
 #define PROGNAME           "smfwatchdog"
-#define VERSION            "0.0.4"
+#define VERSION            "0.0.5"
 
 #define DATEFMT            "%Y-%m-%dT%H:%M:%S"
 #define DEFAULT_MAIL_PROG  "mailx -t"
